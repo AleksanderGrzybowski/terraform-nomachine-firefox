411a9f584a87a03a7e4593ed1ed5ee3ae231efc6
Aleksander Grzybowski <me@aleksandergrzybowski.pl>
2019-10-08 10:01:01 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+Terraform script for provisioning EC2 instance with graphical Xubuntu environment + Firefox, useful for *very* private browsing, totally not at work.
+
+Run `terraform apply`, then connect with X2Go client (user `ubuntu`) using IP printed back at the end.
---
+++ main.tf
@@ -0,0 +1,19 @@
+provider "aws" {
+  region = "eu-central-1"
+}
+
+resource "aws_instance" "scratch" {
+  ami             = "ami-0ac05733838eabc06"
+  instance_type   = "t2.small"
+  key_name        = "agrzybowski"
+  security_groups = ["all-open"]
+  user_data       = "${file("setup.sh")}"
+
+  tags = {
+    Name = "scratch"
+  }
+}
+
+output "instance_ip_addr" {
+  value = aws_instance.scratch.public_ip
+}
