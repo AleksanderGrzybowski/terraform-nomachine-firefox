2a3c90ff2b687fba6f26e540b7c9806746b48dde
Aleksander Grzybowski <me@aleksandergrzybowski.pl>
2020-01-07 17:37:12 +0100

    Improve DigitalOcean integration, password login

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 Terraform script for provisioning EC2 instance with graphical Xubuntu environment + Firefox, useful for *very* private browsing, totally not at work.
 
 Run `terraform apply`, then connect with X2Go client (user `ubuntu`) using IP printed back at the end.
+
+There is also manual bash script for Digital Ocean cloud.
--- digitalocean/create.sh
+++ digitalocean/create.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 . ./common.sh
 
-doctl compute droplet create ${INSTANCE_NAME} --image ubuntu-18-04-x64 --region fra1 --ssh-keys 41:14:e9:86:da:a7:42:c1:af:1d:52:4e:07:3b:57:b7 --size 2gb --user-data-file ../setup.sh
+doctl compute droplet create ${INSTANCE_NAME} --image ubuntu-18-04-x64 --region fra1 --ssh-keys ee:a4:d1:c3:61:b1:7c:33:ce:49:a0:01:c5:a4:3b:09 --size 2gb --user-data-file ../setup.sh
 sleep 20
 doctl compute droplet list | tail -n1 | awk '{print $3}'
 
