868cadef6a81c3019fd9af177e459b5855659bad
Aleksander Grzybowski <ifyouseethis@tellmeplease.it>
2021-05-03 20:03:47 +0200

    New AMI

--- main.tf
+++ main.tf
@@ -47,7 +47,7 @@ resource "aws_security_group" "scratch_security_group" {
 }
 
 resource "aws_instance" "scratch" {
-  ami             = "ami-0502e817a62226e03"
+  ami             = "ami-05f7491af5eef733a"
   instance_type   = "t3a.small"
   key_name        = aws_key_pair.scratch_ssh_key.key_name
   security_groups = [aws_security_group.scratch_security_group.name]
