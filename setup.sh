b6f0ea022c5b26209758e9dfb319f4b77a81fb4a
Aleksander Grzybowski <me@aleksandergrzybowski.pl>
2023-05-06 15:02:22 +0200

    Add more logs

--- setup.sh
+++ setup.sh
@@ -5,11 +5,18 @@ REPO_NAME="terraform-x2go-firefox"
 REPO_URL="https://github.com/AleksanderGrzybowski/${REPO_NAME}.git"
 UBUNTU_PASSWORD="x2goletmein2020"
 
+echo "Cloning ${REPO_URL}..."
+
 git clone ${REPO_URL}
 cd ${REPO_NAME}
+
+echo "Installing Ansible..."
 
 apt-get update
 apt-get -y install ansible
 
+echo "Running playbook..."
+
 ansible-playbook -e "ubuntu_password=${UBUNTU_PASSWORD}" -i ./hosts playbook.yml
 
+echo "Provisioning done."
